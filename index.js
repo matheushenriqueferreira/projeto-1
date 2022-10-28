1ac909ef2a2e36466082da71e03794a3cb19fed4
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-10-28 19:42:10 -0300

    Alterado elementos
    Inserido mais uma div e title

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   const mainContainer = document.querySelector(".mainContainer");
   const loginSection = document.createElement("section");
-  const emailContainer = document.createElement('div');
-  const passwordContainer = document.createElement('div');
+  const loginSectionContent = document.createElement('div');
+  const title = document.createElement("h1");
   const inputEmail = document.createElement("input");
   const inputPassword = document.createElement("input");
   const btnLogin = document.createElement("button");
@@ -12,26 +12,30 @@ window.onload = function() {
 
     loginSection.setAttribute('id', 'loginSection');
 
-    btnLogin.setAttribute('id', 'btnLogin');
-    btnLogin.setAttribute('type', 'button');
-    btnLogin.innerText = 'Entrar'
-    emailContainer.setAttribute('id', 'emailContainer');
-    passwordContainer.setAttribute('id', 'passwordContainer');
-
-    mainContainer.appendChild(loginSection);
-    loginSection.appendChild(emailContainer);
-    loginSection.appendChild(passwordContainer);
-    loginSection.appendChild(btnLogin);
+    loginSectionContent.setAttribute('id', 'loginSectionContent');
     
+    mainContainer.appendChild(loginSection);
+    loginSection.appendChild(loginSectionContent);
+    
+    title.setAttribute('class', 'section2Title');
+    title.innerText = 'Login';
     inputEmail.setAttribute('id', 'userEmail');
     inputEmail.setAttribute('type', 'email');
     inputEmail.setAttribute('placeholder', 'Insira o seu e-mail');
+    inputEmail.setAttribute('class', 'inputStyle');
     inputPassword.setAttribute('id', 'userPassword');
     inputPassword.setAttribute('type', 'password');
     inputPassword.setAttribute('placeholder', 'Insira a sua senha');
+    inputPassword.setAttribute('class', 'inputStyle');
+    btnLogin.setAttribute('id', 'btnLogin');
+    btnLogin.setAttribute('type', 'button');
+    btnLogin.setAttribute('class', 'loginBtnStyle');
+    btnLogin.innerText = 'Entrar'
     
-    emailContainer.appendChild(inputEmail);
-    passwordContainer.appendChild(inputPassword);
+    loginSectionContent.appendChild(title);
+    loginSectionContent.appendChild(inputEmail);
+    loginSectionContent.appendChild(inputPassword);
+    loginSectionContent.appendChild(btnLogin);
   }
   
   document.querySelector('#headerLinkLogin').addEventListener('click', loginContentEvent)
