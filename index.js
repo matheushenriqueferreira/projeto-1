8fe2cb753da97b7e0799d2e1b034f5b5daa2f4f3
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-10-28 17:47:43 -0300

    Removido label e inserido placeholder

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ window.onload = function() {
   const section2 = document.querySelector(".section2Content");
   const emailContainer = document.createElement('div');
   const passwordContainer = document.createElement('div');
-  const labelEmail = document.createElement("label");
   const inputEmail = document.createElement("input");
-  const labelPassword = document.createElement("label");
   const inputPassword = document.createElement("input");
   const btnLogin = document.createElement("button");
 
@@ -20,18 +18,15 @@ window.onload = function() {
     section2.appendChild(emailContainer);
     section2.appendChild(passwordContainer);
     section2.appendChild(btnLogin);
-  
-    labelEmail.innerText = 'E-mail';
-    labelPassword.innerText = 'Senha';
     
     inputEmail.setAttribute('id', 'userEmail');
     inputEmail.setAttribute('type', 'email');
+    inputEmail.setAttribute('placeholder', 'Insira o seu e-mail');
     inputPassword.setAttribute('id', 'userPassword');
     inputPassword.setAttribute('type', 'password');
+    inputPassword.setAttribute('placeholder', 'Insira a sua senha');
     
-    emailContainer.appendChild(labelEmail);
     emailContainer.appendChild(inputEmail);
-    passwordContainer.appendChild(labelPassword);
     passwordContainer.appendChild(inputPassword);
   }
   
