60fe74fc6118a2390347aa52313b163c2241dd22
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-10-28 22:07:24 -0300

    Inserido funcionalidade do localStorage
    Para armazenamento do Token e manter login ao atualizar pagina,
    apagar storage ao clicar em sair

--- index.js
+++ index.js
@@ -6,41 +6,60 @@ window.onload = function() {
   const inputEmail = document.createElement("input");
   const inputPassword = document.createElement("input");
   const btnLogin = document.createElement("button");
+  const headerLinkLogin = document.querySelector("#headerLinkLogin");// Botão Entrar do Header
+  const headerLinkHelp = document.querySelector('#headerLinkHelp');
+  const headerBtnDownload = document.querySelector('#headerBtnDownload');
+
+  const storage = localStorage;
+  const handleLogin = () => {
+    if(storage.getItem('Token')){//Verifica se o Token existe
+      mainContainer.innerHTML = '';
+      headerLinkLogin.innerHTML = 'Sair'
+      headerLinkHelp.remove();
+      headerBtnDownload.remove();
+    }
+  }
 
   const loginContentEvent = () => {
-    mainContainer.innerHTML = '';
-
-    loginSection.setAttribute('id', 'loginSection');
-
-    loginSectionContent.setAttribute('id', 'loginSectionContent');
-    
-    mainContainer.appendChild(loginSection);
-    loginSection.appendChild(loginSectionContent);
-    
-    title.setAttribute('class', 'section2Title');
-    title.innerText = 'Login';
-    inputEmail.setAttribute('id', 'userEmail');
-    inputEmail.setAttribute('type', 'email');
-    inputEmail.setAttribute('placeholder', 'Insira o seu e-mail');
-    inputEmail.setAttribute('class', 'inputStyle');
-    inputPassword.setAttribute('id', 'userPassword');
-    inputPassword.setAttribute('type', 'password');
-    inputPassword.setAttribute('placeholder', 'Insira a sua senha');
-    inputPassword.setAttribute('class', 'inputStyle');
-    btnLogin.setAttribute('id', 'btnLogin');
-    btnLogin.setAttribute('type', 'button');
-    btnLogin.setAttribute('class', 'loginBtnStyle');
-    btnLogin.innerText = 'Entrar'
-    
-    loginSectionContent.appendChild(title);
-    loginSectionContent.appendChild(inputEmail);
-    loginSectionContent.appendChild(inputPassword);
-    loginSectionContent.appendChild(btnLogin);
+    if(headerLinkLogin.innerHTML === 'Entrar') {
+      mainContainer.innerHTML = '';
+      
+      loginSection.setAttribute('id', 'loginSection');
+      
+      loginSectionContent.setAttribute('id', 'loginSectionContent');
+      
+      mainContainer.appendChild(loginSection);
+      loginSection.appendChild(loginSectionContent);
+      
+      title.setAttribute('class', 'section2Title');
+      title.innerText = 'Login';
+      inputEmail.setAttribute('id', 'userEmail');
+      inputEmail.setAttribute('type', 'email');
+      inputEmail.setAttribute('placeholder', 'Insira o seu e-mail');
+      inputEmail.setAttribute('class', 'inputStyle');
+      inputPassword.setAttribute('id', 'userPassword');
+      inputPassword.setAttribute('type', 'password');
+      inputPassword.setAttribute('placeholder', 'Insira a sua senha');
+      inputPassword.setAttribute('class', 'inputStyle');
+      btnLogin.setAttribute('id', 'btnLogin');
+      btnLogin.setAttribute('type', 'button');
+      btnLogin.setAttribute('class', 'loginBtnStyle');
+      btnLogin.innerText = 'Entrar'
+      
+      loginSectionContent.appendChild(title);
+      loginSectionContent.appendChild(inputEmail);
+      loginSectionContent.appendChild(inputPassword);
+      loginSectionContent.appendChild(btnLogin);
+    }
+    else {//Remove o item do LocalStorage e faz o reload da página
+      storage.removeItem('Token');
+      document.location.reload();
+    }
   }
   
-  document.querySelector('#headerLinkLogin').addEventListener('click', loginContentEvent)
+  headerLinkLogin.addEventListener('click', loginContentEvent)
   document.querySelector('#section2LinkLogin').addEventListener('click', loginContentEvent)
-
+  
   btnLogin.addEventListener('click', () => {
     const email = document.querySelector('#userEmail').value;
     const password = document.querySelector('#userPassword').value;
@@ -54,7 +73,8 @@ window.onload = function() {
         "password": password
       })
       .then((resp) => {
-        console.log(resp)
+        storage.setItem('Token', resp.data.token);
+        handleLogin();
       })
       .catch((error) => {
         alert(error);
@@ -62,5 +82,5 @@ window.onload = function() {
     }
   })
 
-
+  handleLogin();
 }
